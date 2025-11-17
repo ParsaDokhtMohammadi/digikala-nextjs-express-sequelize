5bdf2009ab5538fd28814b2d735464a33d82fccf
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-11-17 16:11:13 +0330

    inital app setup completed

---
+++ server/app.js
@@ -0,0 +1,43 @@
+import express from "express"
+import { config } from "dotenv"
+//------------------------------------------
+
+
+const app = express()
+config()
+//------------------------------------------
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+//------------------------------------------
+
+
+
+
+
+
+
+//------------------------------------------
+app.use((req , res , next)=>{
+    return res.status(404).json({
+        message : "route not found"
+    })
+})
+
+app.use((err, req , res , next)=>{
+    const status = err?.status ?? 500
+    const message = err?.message ?? "internal server error"
+    return res.status(status).json({
+        message : message
+    })
+})
+
+
+app.listen(3001,async()=>{
+    try{
+        console.log("server running http://localhost:3001");
+    }catch(err){
+        console.log(err);
+    }  
+})
