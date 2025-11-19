c5daa20fe0b1b1d94c1594a8e53e12009b9cd36b
parsa dokhtmohammadi <parsadokhtmohammadi.work@gmail.com>
2025-11-19 15:15:15 +0330

    product.services created

---
+++ server/modules/Products/product.service.js
@@ -0,0 +1,71 @@
+import createHttpError from "http-errors"
+import { ProductTypes } from "../../common/constants/product.const"
+import { Product, ProductColor, ProductDetail, ProductSize } from "./product.model.js"
+
+export async function createProduct(req , res , next){
+    try{
+        const {title , 
+            price , 
+            discount , 
+            active_discount , 
+            description , 
+            img_url , 
+            details ,
+            colors ,
+            sizes ,
+            count ,
+            type , } = req.body
+            if(!Object.values(ProductTypes).includes(type))throw createHttpError(400 , "invalid product type")
+            const product = await Product.create({
+                title,
+                description,
+                price,
+                discount,
+                active_discount,
+                img_url,
+                type,
+            })
+            if(details && details.length > 0){
+            for(const detail of details){
+                await ProductDetail.create({
+                    key : detail.key,
+                    value : detail.value,
+                    product_id : product.id
+                })}
+            }
+     
+            
+            if(type === ProductTypes.Sizing){
+                if(sizes && sizes.length > 0){
+                    for(const size of sizes){
+                        await ProductSize.create({
+                            size : size.size,
+                            product_id : product.id,
+                            count : size.count,
+                            price : size.price,
+                            discount : size.discount,
+                            active_discount : size.active_discount
+                        })}
+                    }
+            }
+            if(type === ProductTypes.Coloring){
+                if(colors && colors.length > 0){
+                    for(const color of colors){
+                        await ProductColor.create({
+                            color_name : color.color_name,
+                            color_hex : color.color_hex,
+                            product_id : product.id,
+                            count : color.count,
+                            price : color.price,
+                            discount : color.discount,
+                            active_discount : color.active_discount
+                        })}
+                    }
+            }
+    }catch(err){
+        next(err)
+    }
+}
+async function getAllProducts(){
+    
+}
