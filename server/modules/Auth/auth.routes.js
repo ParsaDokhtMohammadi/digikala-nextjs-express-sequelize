25b7915796cb938b88898d087e82d1d5dc986863
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2025-11-21 12:26:55 +0330

    verifyRefreshTokenHandler added

--- server/modules/Auth/auth.routes.js
+++ server/modules/Auth/auth.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express"
 import { checkOtp, sendOtp } from "./auth.service.js"
+import { verifiyRefreshTokenHandler } from "../../common/utils/auth.utils.js"
 
 const AuthRouter = Router()
 
 AuthRouter.post("/sendOtp",sendOtp)
 AuthRouter.post("/checkOtp",checkOtp)
+AuthRouter.post("/refreshTokens",verifiyRefreshTokenHandler)
 
 export {AuthRouter}
